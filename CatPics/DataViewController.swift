8e51323d791bea1edb6f877e78723dbe3670b4fa
Steven Kanceruk <Steve@stevens-air.hsd1.ga.comcast.net>
2016-06-01 22:20:20 -0400

    Initial Commit

---
+++ CatPics/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  CatPics
+//
+//  Created by Steven Kanceruk on 6/1/16.
+//  Copyright © 2016 Steven Kanceruk Inc. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
